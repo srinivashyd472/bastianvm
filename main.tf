26514f7abd5814967230d7db63b7dc73dda3cc6a
srinivas <srinivashyd472@gmail.com>
2024-12-17 12:06:06 +0530

    ipupdate

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "bastian" {
   name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.srg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.49.20.0/24"]
 }
 #pubip config
 resource "azurerm_public_ip" "pubip" {
